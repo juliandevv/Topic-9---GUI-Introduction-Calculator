19f8512a818f4538789bac217c0c46af4d315112
Julian DeVries <DeVrJuli784@GoTVDSB.ca>
2021-11-02 12:14:09 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Topic_9___GUI_Introduction_Assignment_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.number1Tip = new System.Windows.Forms.ToolStripStatusLabel();
+            this.number2Tip = new System.Windows.Forms.ToolStripStatusLabel();
+            this.error = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.error)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.BackColor = System.Drawing.Color.Honeydew;
+            this.lblNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 54F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber1.Location = new System.Drawing.Point(67, 22);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(75, 82);
+            this.lblNumber1.TabIndex = 2;
+            this.lblNumber1.Text = "0";
+            this.lblNumber1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblNumber1.TextChanged += new System.EventHandler(this.lblNumber1_TextChanged);
+            this.lblNumber1.Click += new System.EventHandler(this.lblNumber1_Click);
+            this.lblNumber1.MouseLeave += new System.EventHandler(this.lblNumber1_MouseLeave);
+            this.lblNumber1.MouseHover += new System.EventHandler(this.lblNumber1_MouseHover);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblResult);
+            this.groupBox2.Controls.Add(this.lblOperator);
+            this.groupBox2.Controls.Add(this.lblNumber2);
+            this.groupBox2.Controls.Add(this.lblNumber1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 79);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 126);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Honeydew;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(566, 36);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(178, 63);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "= 0.00";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.BackColor = System.Drawing.Color.Honeydew;
+            this.lblOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperator.Location = new System.Drawing.Point(253, 48);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(0, 51);
+            this.lblOperator.TabIndex = 6;
+            this.lblOperator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblOperator.Click += new System.EventHandler(this.lblOperator_Click);
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.BackColor = System.Drawing.Color.Honeydew;
+            this.lblNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 54F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber2.Location = new System.Drawing.Point(401, 22);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(75, 82);
+            this.lblNumber2.TabIndex = 4;
+            this.lblNumber2.Text = "0";
+            this.lblNumber2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNumber2.Click += new System.EventHandler(this.lblNumber2_Click);
+            this.lblNumber2.MouseLeave += new System.EventHandler(this.lblNumber2_MouseLeave);
+            this.lblNumber2.MouseHover += new System.EventHandler(this.lblNumber2_MouseHover);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCalculate.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCalculate.FlatAppearance.BorderSize = 3;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(638, 19);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(106, 45);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Honeydew;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/",
+            "avg",
+            "sqrt",
+            "sin",
+            "cos"});
+            this.comboBox1.Location = new System.Drawing.Point(235, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(78, 33);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.BackColor = System.Drawing.Color.Honeydew;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(283, 9);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(190, 37);
+            this.lblHeader.TabIndex = 2;
+            this.lblHeader.Text = "Calculator +";
+            this.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblHeader.Click += new System.EventHandler(this.lblHeader_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.input2);
+            this.groupBox1.Controls.Add(this.input1);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 207);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 77);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(376, 31);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(100, 20);
+            this.input2.TabIndex = 9;
+            this.input2.TextChanged += new System.EventHandler(this.input2_TextChanged);
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(42, 31);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(100, 20);
+            this.input1.TabIndex = 5;
+            this.input1.TextChanged += new System.EventHandler(this.input1_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.number1Tip,
+            this.number2Tip});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // number1Tip
+            // 
+            this.number1Tip.Name = "number1Tip";
+            this.number1Tip.Size = new System.Drawing.Size(60, 17);
+            this.number1Tip.Text = "Number 1";
+            this.number1Tip.Visible = false;
+            // 
+            // number2Tip
+            // 
+            this.number2Tip.Name = "number2Tip";
+            this.number2Tip.Size = new System.Drawing.Size(60, 17);
+            this.number2Tip.Text = "Number 2";
+            this.number2Tip.Visible = false;
+            // 
+            // error
+            // 
+            this.error.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.groupBox2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.error)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel number1Tip;
+        private System.Windows.Forms.ToolStripStatusLabel number2Tip;
+        private System.Windows.Forms.ErrorProvider error;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_9___GUI_Introduction_Assignment_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblResult.Visible = true;
+        }
+
+        string sign;
+        double result;
+        double num1, num2;
+        Boolean err;
+       
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            string num1 = input1.Text;
+            lblNumber1.Text = num1.ToString();
+        }
+
+        private void lblHeader_Click(object sender, EventArgs e)
+        {
+            lblHeader.Location = new Point(ClientSize.Width / 2, 9);
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNumber1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNumber2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            string num2 = input2.Text;
+            lblNumber2.Text = num2.ToString();
+        }
+
+        private void lblOperator_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            error.Clear();
+            sign = comboBox1.Text;
+            if (sign == "sqrt")
+            {
+                lblNumber2.Visible = false;
+                input2.Visible = false;
+                lblNumber1.Visible = true;
+                input1.Visible = true;
+                num2 = 0;
+                input2.Text = "0";
+            }
+
+            else if (sign == "sin")
+            {
+                lblNumber1.Visible = false;
+                input1.Visible = false;
+                lblNumber2.Visible = true;
+                input2.Visible = true;
+                num1 = 0;
+                input1.Text = "0";
+            }
+
+            else if (sign == "cos")
+            {
+                lblNumber1.Visible = false;
+                input1.Visible = false;
+                lblNumber2.Visible = true;
+                input2.Visible = true;
+                num1 = 0;
+                input1.Text = "0";
+            }
+
+            else
+            {
+                lblNumber1.Visible = true;
+                input1.Visible = true;
+                lblNumber2.Visible = true;
+                input2.Visible = true;
+            }
+
+            lblOperator.Text = sign;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               num1 = Convert.ToDouble(input1.Text);
+            } 
+
+            catch (FormatException)
+            {
+                num1 = 0;
+                error.SetError(input1, "That is not an integer!");
+                err = true;
+            }
+
+            try
+            {
+                num2 = Convert.ToDouble(input2.Text);
+            }
+            
+            catch (FormatException)
+            {
+                num2 = 0;
+                error.SetError(input2, "That is not an integer!");
+                err = true;
+            }
+
+
+            if (sign == "+")
+            {
+                result = (num1 + num2);
+            }
+
+            else if (sign == "-")
+            {
+                result = (num1 - num2);
+            }
+
+            else if (sign == "*")
+            {
+                result = (num1 * num2);
+            }
+
+            else if (sign == "/")
+            {
+                result = (num1 / num2);
+            }
+
+            else if (sign == "avg")
+            {
+                result = ((num1 + num2) / 2);
+            }
+
+            else if (sign == "sqrt")
+            {
+                result = (Math.Sqrt(num1));
+            }
+
+            else if (sign == "sin")
+            {
+                result = (Math.Sin(num2));
+            }
+
+            else if (sign == "cos")
+            {
+                result = (Math.Cos(num2));
+            }
+
+            else
+            {
+                error.SetError(comboBox1, "No operator selected!");
+            }
+
+            if (err == true)
+            {
+                lblResult.Text = "error!";
+                err = false;
+            }
+
+            else
+            {
+                result = Math.Round(result, 2);
+                lblResult.Text = "= " + result.ToString();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void statusStrip1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNumber1_MouseHover(object sender, EventArgs e)
+        {
+            number1Tip.Visible = true;
+        }
+
+        private void lblNumber2_MouseHover(object sender, EventArgs e)
+        {
+            number2Tip.Visible = true;
+        }
+
+        private void lblNumber1_MouseLeave(object sender, EventArgs e)
+        {
+            number1Tip.Visible = false;
+        }
+
+        private void input2_TextChanged(object sender, EventArgs e)
+        {
+            lblResult.Visible = true;
+            error.Clear();
+            lblNumber2.Text = input2.Text;
+        }
+
+        private void lblNumber1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void input1_TextChanged(object sender, EventArgs e)
+        {
+            lblResult.Visible = true;
+            lblNumber1.Text = input1.Text;
+            error.Clear();
+        }
+
+        private void lblNumber2_MouseLeave(object sender, EventArgs e)
+        {
+            number2Tip.Visible = false;
+        }
+    }
+}
